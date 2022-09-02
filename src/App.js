cf43d674185299b9976a55d639768f67ec7e2eb9
EduPZz <eduardo.pozzogood@hotmail.com>
2022-09-02 14:56:23 +0000

    Dark and light theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
 import Layout from './components/Layout';
 
+import commonStyle from './styles/themes/common';
+import themes from './styles/themes';
+
 function App() {
+
+  const [theme, setTheme] = useState('dark');
+
+  const curentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme])
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  }
+
   return (
-    <>
+    <ThemeProvider theme={curentTheme}>
       <GlobalStyle />
-      <Layout />
-    </>
+      <Layout 
+        onToggleTheme={handleToggleTheme}
+        selectedTheme={theme}  
+      />
+    </ThemeProvider>
   );
 };
 
